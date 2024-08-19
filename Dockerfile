e4ab943eb873076200ca2d4c276327bf9a9adaf7
junaid-repo <junaidraza3001@gmail.com>
2024-08-19 18:40:02 +0530

    docker updates

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
-FROM openjdk:17-jdk-alpine
-
-COPY target/swigato-0.0.1-SNAPSHOT.jar swigato-1.0.0.jar
-
+FROM openjdk:17
+ADD target/swigato-0.0.1-SNAPSHOT.jar swigato-1.0.0.jar
+EXPOSE 8088
 ENTRYPOINT [ "java", "-jar", "swigato-1.0.0.jar" ]
--- src/main/java/com/food/swigato/api/ApiCalls.java
+++ src/main/java/com/food/swigato/api/ApiCalls.java
@@ -23,7 +23,7 @@ public class ApiCalls {
 	public Map<String, Object> getProductDetails(String id) {
 		// List<GroupMembers> memberList= new ArrayList<>();
 
-		String uri = "http://" + "localhost:7070" + "/resturant/get/foodDetails/";
+		String uri = "http://" + "host.docker.internal:8086" + "/resturant/get/foodDetails/";
 		ResponseEntity<Map<String, Object>> foodDetails = template.exchange(uri + id, HttpMethod.GET,
 				new HttpEntity<>(httpHeader()), new ParameterizedTypeReference<Map<String, Object>>() {
 				});
@@ -40,7 +40,7 @@ public class ApiCalls {
 	}
 
 	public Map<String, Object> getAddress(Integer id) {
-		String uri = "http://" + "host.docker.internal:7072" + "/customer/view/address?id=";
+		String uri = "http://" + "host.docker.internal:8087" + "/customer/view/address?id=";
 		String finalUri=uri+id;
 		System.out.println(finalUri);
 		ResponseEntity<Map<String, Object>> addressDetails = template.exchange(uri + id, HttpMethod.GET,
@@ -49,7 +49,7 @@ public class ApiCalls {
 		return addressDetails.getBody();
 	}
 	public Map<String, Object> getCustomerDetails(String userCode) {
-		String uri = "http://" + "host.docker.internal:7072" + "/customer/view/customerDetails?userCode=";
+		String uri = "http://" + "host.docker.internal:8087" + "/customer/view/customerDetails?userCode=";
 		String finalUri=uri+userCode;
 		System.out.println(finalUri);
 		ResponseEntity<Map<String, Object>> addressDetails = template.exchange(uri + userCode, HttpMethod.GET,
