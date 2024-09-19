1291f772e9f2afc018b4ce0c69f3ef5759eb9502
junaid <junaid@Junaids-Device>
2024-09-19 13:45:40 +0530

    implementing load balancer

--- src/main/java/com/food/swigato/SwigatoApplication.java
+++ src/main/java/com/food/swigato/SwigatoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,10 +16,11 @@ public class SwigatoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SwigatoApplication.class, args);
 	}
-	
-@Bean
-RestTemplate restTemplate() {
-	return new RestTemplate();
-}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 }
--- src/main/java/com/food/swigato/api/ApiCalls.java
+++ src/main/java/com/food/swigato/api/ApiCalls.java
@@ -23,6 +23,8 @@ public class ApiCalls {
 	@Autowired
 	RestTemplate template;
 	
+
+	
 	@Value("${swigato.host}")
 	private String host;
 	
@@ -35,7 +37,7 @@ public class ApiCalls {
 	public Map<String, Object> getProductDetails(String id) {
 		// List<GroupMembers> memberList= new ArrayList<>();
 
-		String uri = "http://" + host+":"+resturantPort + "/resturant/get/foodDetails/";
+		String uri = "http://" + "RESTURANTS-SERVICE" + "/resturant/get/foodDetails/";
 		System.out.println("The uri for getProductDetails "+ uri);
 		ResponseEntity<Map<String, Object>> foodDetails = template.exchange(uri + id, HttpMethod.GET,
 				new HttpEntity<>(httpHeader()), new ParameterizedTypeReference<Map<String, Object>>() {
@@ -53,7 +55,8 @@ public class ApiCalls {
 	}
 
 	public Map<String, Object> getAddress(Integer id) {
-		String uri = "http://" + host+":"+customersPort + "/customer/view/address?id=";
+		//String uri = "http://" + host+":"+customersPort + "/customer/view/address?id=";
+		String uri = "http://" +"CUSTOMERS-SERVICE"+"/customer/view/address?id=";
 		//log.info("The uri for getAddress "+ uri);
 		String finalUri=uri+id;
 		System.out.println("The uri for getAddress "+finalUri);
@@ -63,7 +66,7 @@ public class ApiCalls {
 		return addressDetails.getBody();
 	}
 	public Map<String, Object> getCustomerDetails(String userCode) {
-		String uri =  "http://" + host+":"+customersPort  + "/customer/view/customerDetails?userCode=";
+		String uri =  "http://" +"CUSTOMERS-SERVICE"+ "/customer/view/customerDetails?userCode=";
 		String finalUri=uri+userCode;
 		System.out.println("The uri for getCustomerDetails "+finalUri);
 		ResponseEntity<Map<String, Object>> addressDetails = template.exchange(uri + userCode, HttpMethod.GET,
